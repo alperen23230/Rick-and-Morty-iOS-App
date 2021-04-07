ffac3dfe731dd24e35e1878a4d786c6ffc61bed9
Alperen Ünal <alperenunal68@gmail.com>
2021-04-07 10:22:44 +0300

    Network service tests is done

--- RickAndMortyiOS/RickAndMortyiOSTests/NetworkServiceTests.swift
+++ RickAndMortyiOS/RickAndMortyiOSTests/NetworkServiceTests.swift
@@ -12,15 +12,15 @@ import Combine
 class NetworkServiceTests: XCTestCase {
 
     private var cancellables = Set<AnyCancellable>()
-    var expectationAPIResponse: XCTestExpectation!
-    let timeoutForAPIResponse = 2.0
+    private var expectationAPIResponse: XCTestExpectation!
+    private let timeoutForAPIResponse = 5.0
 
-    override func setUpWithError() throws {
+    override func setUp() {
         expectationAPIResponse = expectation(description: "API respond on time")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        expectationAPIResponse = nil
     }
 
     func test_get_characters() {
@@ -29,6 +29,36 @@ class NetworkServiceTests: XCTestCase {
         NetworkService.sharedInstance.getCharacters(for: 1, filterByName: "", filterByGender: "", filterByStatus: "").sink { (completion) in
             if case .failure(_) = completion {
                 XCTFail("API return error on first page of characters")
+            }
+        } receiveValue: { [weak self] (response) in
+            guard let self = self else { return XCTFail() }
+            XCTAssertNotNil(response)
+            do { self.expectationAPIResponse.fulfill() }
+        }
+            .store(in: &cancellables)
+    }
+
+    func test_get_episodes() {
+        defer { waitForExpectations(timeout: timeoutForAPIResponse) }
+
+        NetworkService.sharedInstance.getEpisodes(for: 1, filterByName: "").sink { (completion) in
+            if case .failure(_) = completion {
+                XCTFail("API return error on first page of episodes")
+            }
+        } receiveValue: { [weak self] (response) in
+            guard let self = self else { return XCTFail() }
+            XCTAssertNotNil(response)
+            do { self.expectationAPIResponse.fulfill() }
+        }
+            .store(in: &cancellables)
+    }
+
+    func test_get_locations() {
+        defer { waitForExpectations(timeout: timeoutForAPIResponse) }
+
+        NetworkService.sharedInstance.getLocations(for: 1, filterByName: "").sink { (completion) in
+            if case .failure(_) = completion {
+                XCTFail("API return error on first page of locations")
             }
         } receiveValue: { [weak self] (response) in
             guard let self = self else { return XCTFail() }
